9d6faff633341a937b03de5554969b120692123b
Cliry1 <128536194+Cliry1@users.noreply.github.com>
2024-01-16 23:52:44 +0200

    new

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
@@ -5,58 +6,100 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const form = document.querySelector(".form");
+const buttonLoad =document.querySelector(".button");
 const ul = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
+const btnMore = document.querySelector(".button-load-more");
+
+let searchInfo;
+let pageCount;
+const limit = 40;
+let totalItems;
 
 var lightbox = new SimpleLightbox('.gallery a', {   
   captionsData: 'alt',
   captionDelay: 250, }); 
-form.addEventListener("submit", createListItem)
+form.addEventListener("submit", createListItem);
 
 
-function createListItem(event){
-  event.preventDefault();
-  ul.innerHTML = "";
-  const itemParam = new URLSearchParams({
-    key:"41728524-9ad3c3555a09648a5caa6ae20",
-    q:form.item.value.trim(),
-    image_type : "photo",
-    orientation :"horizontal",
-    safesearch : true
-  });
-  loader.classList.remove("search");
-  fetch(`https://pixabay.com/api/?${itemParam}`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Request error");
-    }
-    return response.json();
-  })
-  .then(result => {
-    if (result.hits.length === 0) {
-      iziToast.error({
-        title: 'No result',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position:"topRight"
+btnMore.addEventListener("click", async ()=>{
+  try {
+    btnMore.classList.add('search');
+    pageCount++;
+    const posts = await paramList(pageCount,searchInfo);
+    createImg(posts);
+    if(pageCount>=Math.ceil(totalItems/limit)){
+      btnMore.classList.add("search");
+      return iziToast.error({
+        position: "topRight",
+        message: "We're sorry, but you've reached the end of search results."
       });
     }
-    console.log(result);
-    const markup = result.hits.reduce((acc, elem)=>
-      acc + `<li class="element"><a href="${elem.largeImageURL}"><img class="image" src="${elem.webformatURL}"  alt="${elem.tags}"></a>
-      <div class="container-stat">
-      <p class="stat">Likes <span class="stat-value">${elem.likes}</span></p>
-      <p class="stat">Views <span class="stat-value">${elem.views}</span></p>
-      <p class="stat">Comments <span class="stat-value">${elem.comments}</span></p>
-      <p class="stat">Downloads <span class="stat-value">${elem.downloads}</span></p>
-      </div></li>`, "")
+    else{
+      btnMore.classList.remove('search');
+    }
 
-    ul.insertAdjacentHTML("beforeend", markup);
-    lightbox.refresh();
-  })
-  .catch(error => console.log(error))
-  .finally(()=>{
-    loader.classList.add('search');
-    form.reset();
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+
+async function paramList(pageCount, searchInfo){
+  const itemParam = new URLSearchParams({
+    key:"41728524-9ad3c3555a09648a5caa6ae20",
+    q:searchInfo,
+    image_type : "photo",
+    orientation :"horizontal",
+    safesearch : true,
+    page : pageCount,
+    per_page: limit
   });
+  const collection = await axios.get(`https://pixabay.com/api/?${itemParam}`)
+  return collection.data;
+}
+function createImg(posts){
+  console.log(posts);
+  if(posts.total === 0){
+    iziToast.error({
+      title: 'No result',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position:"topRight"
+    });
+    loader.classList.add('search');
+    return;
+  }
+  const markup = posts.hits.reduce((acc, elem)=>
+  acc + `<li class="element"><a href="${elem.largeImageURL}"><img class="image" src="${elem.webformatURL}"  alt="${elem.tags}"></a>
+  <div class="container-stat">
+  <p class="stat">Likes <span class="stat-value">${elem.likes}</span></p>
+  <p class="stat">Views <span class="stat-value">${elem.views}</span></p>
+  <p class="stat">Comments <span class="stat-value">${elem.comments}</span></p>
+  <p class="stat">Downloads <span class="stat-value">${elem.downloads}</span></p>
+  </div></li>`, "")
+ 
+  ul.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
+  loader.classList.add('search');
+  totalItems = posts.totalHits;
+
+}
+async function createListItem(event){
+  event.preventDefault();
+  searchInfo= form.item.value.trim();
+  ul.innerHTML = ""
+  pageCount=1;
+  loader.classList.remove("search");
+  const posts = await paramList(pageCount, searchInfo);
+  createImg(posts);
+  form.reset();
+  if(totalItems>40){
+    btnMore.classList.remove('search');
+  }
+  else{
+    btnMore.classList.add('search');
+  }
 }
